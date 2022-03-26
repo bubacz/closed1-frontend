312c22579479d00f84e22214d439a250c1ac62da
Kevin Bubacz <kevinb989@gmail.com>
2022-03-26 17:54:49 -0400

    Tightened up spacing of post and comments

--- components/Comments.js
+++ components/Comments.js
@@ -1,33 +1,42 @@
 import React, { Component } from "react";
 import Comment from "./Comment";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const CommentsList = styled.div`
-  display: grid;
-  padding: 1rem 0;
+	display: grid;
+	padding: 1rem 0;
+
+  .divider {
+    opacity: 0.25;
+    width: calc(100% - 4rem);
+    margin: 0 auto;
+  }
 `;
 
 class Comments extends Component {
-  render() {
-    const { commentList, me } = this.props;
-    return (
-      <CommentsList>
-        {commentList.length
-          ? commentList.map((comment) => {
-              return (
-                <Comment
-                  me={me}
-                  key={comment.id}
-                  comment={comment}
-                  mainCommentId={comment.id}
-                  replyComment={this.props.replyComment}
-                />
-              );
-            })
-          : ""}
-      </CommentsList>
-    );
-  }
+	render() {
+		const { commentList, me } = this.props;
+		return (
+			<CommentsList>
+				{commentList.length
+					? commentList.map(comment => {
+							return (
+								<>
+									<hr class="divider" />
+									<Comment
+										me={me}
+										key={comment.id}
+										comment={comment}
+										mainCommentId={comment.id}
+										replyComment={this.props.replyComment}
+									/>
+								</>
+							);
+					  })
+					: ""}
+			</CommentsList>
+		);
+	}
 }
 
 export default Comments;
